88255e87eb8c9525bdea36ec0e2979d4874d5319
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2020-05-20 14:16:25 +0700

    Updates

---
+++ precache-manifest.647261306964e5aa300862991a071329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20001372147c8df415ae2950b2740ef0",
+    "url": "/ghtest/index.html"
+  },
+  {
+    "revision": "78b2250cfc8abea24782",
+    "url": "/ghtest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16450e3f50768d0ea7f8",
+    "url": "/ghtest/static/js/2.710e1bdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghtest/static/js/2.710e1bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b2250cfc8abea24782",
+    "url": "/ghtest/static/js/main.ed70098e.chunk.js"
+  },
+  {
+    "revision": "9d2d31f2730292843723",
+    "url": "/ghtest/static/js/runtime-main.4bdb7694.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
