7e3459284fdbfc28ec42cb6b4c2bb71b97398ca1
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2020-05-20 14:20:52 +0700

    Updates

---
+++ precache-manifest.cc037a203214ab6664ee7e33c0a932c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112fae503e8e7ef2c986f175e3f7a720",
+    "url": "/ghtest/index.html"
+  },
+  {
+    "revision": "d2fc3dd2b469a05b2309",
+    "url": "/ghtest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b1455f332b957b3b998a",
+    "url": "/ghtest/static/js/2.163b876d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ghtest/static/js/2.163b876d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fc3dd2b469a05b2309",
+    "url": "/ghtest/static/js/main.a69da867.chunk.js"
+  },
+  {
+    "revision": "9d2d31f2730292843723",
+    "url": "/ghtest/static/js/runtime-main.4bdb7694.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ghtest/precache-manifest.647261306964e5aa300862991a071329.js"
+  "/ghtest/precache-manifest.cc037a203214ab6664ee7e33c0a932c7.js"
 );
 
 self.addEventListener('message', (event) => {
